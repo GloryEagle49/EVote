0a7eb29a43f81721fe80c8d04a1310125192ed2c
ebuberoderick <ebuberoderick2@gmail.com>
2021-11-02 22:19:24 +0100

    added some libraries

--- backend/nav.php
+++ backend/nav.php
@@ -1,4 +1,10 @@
-<?php
-    include_once 'connect.php';
-    echo ' hi';
+<?php 
+    include_once 'backend/connect.php';
+    session_start();
+    if(!isset($_SESSION['active_user_id'])){
+        header('location:index.html');
+    };
+    $user_id=$_SESSION['active_user_id'];
+    $query_my_data = $conn->query("SELECT * FROM users WHERE sn='$user_id'");
+    $my_data= $query_my_data->fetch_array();
 ?>
---
+++ backend/qureies.php
@@ -0,0 +1,4 @@
+<?php
+    include_once 'backend/connect.php';
+    session_start();
+?>
